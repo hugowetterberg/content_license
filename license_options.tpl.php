16cd7f628f89f33150f9c12c9a2770ae4f7eb35c
Hugo Wetterberg <hugo.wetterberg@gmail.com>
2009-04-14 09:52:16 +0200

    Initial commit

---
+++ license_options.tpl.php
@@ -0,0 +1,32 @@
+<div id="<?php print $form['#id']; ?>-wrapper" class="content-licenses">
+<?php
+  foreach ($form['#options'] as $lid => $title) {
+    $license = $lid!='none' ? $form['#licenses'][$lid] : (object)array(
+      'license_badge' => url(drupal_get_path('module', 'content_license') . '/copyrighted.png'),
+    );
+    $id = $form['#id'] . '-' . $lid;
+    ?>
+    <div class="license">
+      <div class="select">
+        <input type="<?php print $widget ?>" id="<?php print $id ?>" name="<?php print $form['#id']; 
+          ?>" value="<?php print $lid ?>" <?php 
+          print in_array($lid, $form['#default_value']) ? 'checked="checked"' : '' ?>/>
+        <label for="<?php print $id ?>">
+          <?php print $title ?>
+        </label>
+      </div>
+      <?php if (!empty($license->license_badge)): ?>
+      <img src="<?php print $license->license_badge ?>" class="badge" />
+      <?php endif; ?>
+      <p class="description">
+        <?php print $license->license_description ?>
+      </p>
+      <div class="links">
+        <?php if (!empty($license->license_url)): ?>
+        <a href="<?php print $license->license_url ?>" target="_blank">Read more about the license here</a>
+        <?php endif; ?>
+      </div>
+    </div><?php
+  }
+?>
+</div>
